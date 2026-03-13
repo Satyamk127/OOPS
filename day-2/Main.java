48d1772e11ef2ad6bc32d166154a7cae0c19a22b
Satyamkeshari <satyamkumarkeshari358@gmail.com>
2026-03-13 09:34:23 +0530

    oops

---
+++ day-2/Main.java
@@ -0,0 +1,11 @@
+class Main{
+    public static void main(String [] args){
+        Student s1 = new Student();
+        s1.StudentId = 101;
+        s1.StudentName = "Johan"; 
+        s1.StudentAddress = "Delhi";
+        s1.StudentStudy();
+        s1.Studentdetails();
+    
+    }
+}
---
+++ day-2/Student.java
@@ -0,0 +1,21 @@
+class  Student{
+
+  // instace variable or data member
+  public int StudentId;
+  public String StudentName;
+  public String StudentAddress;
+
+  // Method 
+  public void StudentStudy(){
+    System.out.println(StudentName+" is Studying");
+  }
+  public void Studentdetails(){
+    System.out.println("Student Id : "+StudentId);
+    System.out.println("Student Name : "+StudentName);
+    System.out.println("Student Address : "+StudentAddress);
+  } 
+
+
+
+
+}
